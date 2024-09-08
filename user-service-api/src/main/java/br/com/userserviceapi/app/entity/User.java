ed42c598b1379046f6f48c5894b45a43359b2514
Lucas Santos <lucas201273@gmail.com>
2024-09-08 18:50:34 -0300

    feat: create user entity

---
+++ user-service-api/src/main/java/br/com/userserviceapi/app/entity/User.java
@@ -0,0 +1,33 @@
+package br.com.userserviceapi.app.entity;
+
+import br.com.lucas.models.enums.ProfileEnum;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@With
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profileEnum;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
