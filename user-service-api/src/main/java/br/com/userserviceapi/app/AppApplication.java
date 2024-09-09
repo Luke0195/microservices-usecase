123ce4366df43bbb29e6b038f557d26d5471e2fe
Lucas Santos <lucas201273@gmail.com>
2024-09-09 05:11:44 -0300

    feat: add user repository

--- user-service-api/src/main/java/br/com/userserviceapi/app/AppApplication.java
+++ user-service-api/src/main/java/br/com/userserviceapi/app/AppApplication.java
@@ -1,10 +1,11 @@
 package br.com.userserviceapi.app;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
